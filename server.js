9113ee1f0ebd247a3a8205c9eb6a11b138e70ff5
guofeng <one778@163.com>
2018-07-11 09:31:26 +0800

    1

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('您好， 我是 大白');
 });
 
 server.listen(port, hostname, () => {
